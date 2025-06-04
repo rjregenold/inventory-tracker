3ca7371e48368f03b6839f0af8ce1fb781530bf9
RJ Regenold <rj@up2code.io>
2025-06-04 05:41:53 -0500

    cleans up api app lifecycle.
    * adds nestjs-graceful-shutdown to handle signals and allow app to
      shutdown cleanly.
    * adds concurrently so ctrl+c is propagated to api and client correctly.
    * disconnects prisma connection on app shutdown.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {Module} from '@nestjs/common';
+import { GracefulShutdownModule } from 'nestjs-graceful-shutdown';
 
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
@@ -6,6 +7,7 @@ import {ParentItemsModule} from "../parent-items/parent-items.module";
 
 @Module({
   imports: [
+    GracefulShutdownModule.forRoot(),
     ParentItemsModule,
   ],
   controllers: [AppController],
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { setupGracefulShutdown } from 'nestjs-graceful-shutdown';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.enableShutdownHooks();
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+
+  // handles signals (SIGINT, SIGTERM) and gives the
+  // app an opportunity to shutdown cleanly
+  setupGracefulShutdown({ app })
+
   const port = process.env.PORT || 3100;
   await app.listen(port);
   Logger.log(
