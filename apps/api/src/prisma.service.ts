ce61c72602b3084a653dfac743340562777ebcb6
RJ Regenold <rj@up2code.io>
2025-06-04 07:22:02 -0500

    enables prisma query logging

--- apps/api/src/prisma.service.ts
+++ apps/api/src/prisma.service.ts
@@ -1,8 +1,17 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import {PrismaClient} from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // log queries. in a production app, we would enable/disable
+    // this based on environment.
+    super({log: ['query']});
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
