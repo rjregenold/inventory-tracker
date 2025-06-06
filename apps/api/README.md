1c08c2194998289318493025b3022aaa37fdd842
RJ Regenold <rj@up2code.io>
2025-06-06 02:04:37 -0500

    fixes from initial self-review of pr

---
+++ apps/api/README.md
@@ -0,0 +1,13 @@
+# Inventory API
+
+This directory contains the api code for the application.
+
+## Setup
+
+1. Copy environment variables:
+   ```bash
+   cp .env.example .env
+  ```
+2. Update `.env` with real values.
+
+
--- apps/client/app/not-found.tsx
+++ apps/client/app/not-found.tsx
@@ -1,7 +1,3 @@
 export default function NotFound() {
-  return (
-    <>
-      <div>The page you are looking for could not be found.</div>
-    </>
-  );
+  return <div>The page you are looking for could not be found.</div>;
 }
